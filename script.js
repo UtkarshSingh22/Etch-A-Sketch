0bc40e9a3f0ee7e4f2de4d318fd2c33302f8a7fe
Utkarsh Singh <sutkarsh453@gmail.com>
2022-04-09 16:26:20 +0530

    Add grid in js

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-function makeGrid(size){
-
+function makeGrid(size = 10){
+    
     for(let i = 0; i < size; i++){
 
+        const len = 500/size;
         const row = document.createElement('div');
         row.className = 'row';
         row.classList.add = 'row';
+        row.style.height = len+'px';
 
         for(let j = 0; j < size; j++){
             const element = document.createElement('div');
             element.className = 'ele';
             element.classList.add = 'ele';
+            element.style.width = len+'px' ;
             row.appendChild(element);
         }
         grid.appendChild(row);
-        //console.log(row);
     }
 }
 
 const grid = document.querySelector('.grid');
 
-makeGrid(10);
+makeGrid();
